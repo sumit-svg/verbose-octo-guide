ff602c7a2e1fbf7cb4ed54e1d45695bd7b74bb6d
sumit-svg <nikoliyaff@gmail.com>
2021-05-30 13:47:35 +0530

    hello

---
+++ javascript/script.js
@@ -0,0 +1,12 @@
+
+// navigation function script start
+function myFunction(){
+    var x = document.getElementById("menu-bar");
+    if(x.style.display == "block"){
+        x.style.display == "none";
+    }
+    else{
+        x.style.display = "block";
+    }
+}
+// navigation function script end
